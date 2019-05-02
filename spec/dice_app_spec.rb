63c237d5322701d10cd0a3252d5b417aa6256629
Student <student@MAKERSs-MacBook-Pro.local>
2019-05-02 12:47:29 +0100

    created #roll method and tested, refactored to one line test

--- spec/dice_app_spec.rb
+++ spec/dice_app_spec.rb
@@ -1,6 +1,12 @@
 require 'dice_app'
 
 
-describe Dice do 
-
+describe Dice do
+  describe "#roll" do
+    # it "rolls the dice" do
+      # dice = Dice.new
+      # expect(subject).to respond_to(:roll)
+    # end
+    it { is_expected.to respond_to(:roll) }
+  end
 end
